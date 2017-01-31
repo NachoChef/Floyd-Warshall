977e8d1ddce889b95d129e5bdda6b75472d0ec91
Justin Jones <hock3ydud3@gmail.com>
2017-01-31 10:09:26 -0600

    Update genericArray.adb

--- genericArray.adb
+++ genericArray.adb
@@ -1,6 +1,8 @@
 generic
   type subscript is (<>);
   type myType is private;
+  with function "OR" (X, Y : myType) return myType;
+  with procedure Put(X : myType);
 package genericArray is
   type userDefinedArray is array(subscript range <>, subscript range <>) of myType;
 end genericArray;
