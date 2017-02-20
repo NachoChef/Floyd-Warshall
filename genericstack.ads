35e67c1c7cc183664554191a56e6a51b19962f49
Justin Jones <hock3ydud3@gmail.com>
2017-02-19 19:53:07 -0600

    Delete genericstack.ads

--- genericstack.ads
+++
@@ -1,8 +0,0 @@
-generic
-  max:integer;
-  type item is private;
-package genericstack is
-   procedure push(x: in item);
-   procedure pop(x: out item);
-   function isEmpty return Boolean;
-end genericstack;
