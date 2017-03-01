8b9cef087a4e330a2a8be9c9a5257095e8affd85
Justin Jones <jxj037@shsu.edu>
2017-03-01 15:04:04 -0600

    All outputs complete

--- main.adb
+++ main.adb
@@ -9,9 +9,10 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure main is   
 begin
    declare
-      --discrete subtypes for generic
+      --discrete subtypes for generic instantiation
       subtype myString is String(1..3);
       subtype myChar is Character;
+      subtype myInt is Integer;
       
       --overload for bmr elements (0 or 1)
       function "OR" (X, Y : integer) return integer is
@@ -35,6 +36,12 @@ begin
          Ada.Text_IO.Put(File => outFile, Item => ("    " & X));
       end myPut;
       
+      --IO overload for integer
+      procedure myPut (outFile : File_Type; X : myInt) is
+      begin
+         Ada.Text_IO.Put(File => outFile, Item => ("    " & Integer'Image(X)));
+      end myPut;
+      
       --parses integer out of string for generic
       function eval (X: myString) return integer is
       begin
@@ -46,11 +53,21 @@ begin
       begin
          return Character'Pos(X) - 48;
       end eval;
+      
+      --returns integer (unnecessary but required for generic instantiation)
+      function eval (X : myInt) return integer is
+      begin
+         return X;
+      end eval;
   
+      --instantiation
       package myStringBMR is new warshallBMR (myString, myPut, "OR", eval);    
       package myCharBMR is new warshallBMR (myChar, myPut, "OR", eval);  
+      package myIntBMR is new warshallBMR (myInt, myPut, "OR", eval);
    begin
       myStringBMR.construct("a_input1.txt", "a_output.txt");
-      myCharBMR.construct("a_input2.txt", "a_output2.txt");
+      myIntBMR.construct("a_input2.txt", "a_output2.txt");
+      myCharBMR.construct("b_input.txt", "b_output.txt");
+      myCharBMR.construct("c_input.txt", "c_output.txt");
    end; 
 end main;
